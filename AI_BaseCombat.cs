fd4e6f3fc9d1df1b8a1e6af93403311010d6dce1
xuexi <wo3030643991@qq.com>
2023-01-03 21:16:39 +0800

    first commit

---
+++ AI_BaseCombat.cs
@@ -0,0 +1,21 @@
+﻿/********************************************************************************
+ *	功能说明： 客户端战斗AI
+*********************************************************************************/
+using UnityEngine;
+using System.Collections;
+
+
+public class AI_BaseCombat : BaseAI
+{
+    public AI_BaseCombat()
+    {
+        //m_AIType = CharacterDefine.AI_TYPE.AI_TYPE_COMBAT;
+        //AIStateType = CharacterDefine.AI_STATE_TYPE.AI_STATE_COMBAT;
+    }
+
+    public override void UpdateAI()
+    {
+        base.UpdateAI();
+    }
+}
+
